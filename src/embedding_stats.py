f1d7b3fc9e3b36b898a131abaa8a97c72fd52b13
Francesco Gullo <francesco.gullo@unicredit.eu>
2021-01-05 11:41:32 +0100

    update in embedding stats

--- src/embedding_stats.py
+++ src/embedding_stats.py
@@ -1,20 +1,47 @@
 import sys, getopt, os, codecs
 import load
 
-#squared Euclidean distance
-def get_dist(v1,v2):
-    d = 0.0
-    for i in range(0,len(v1)):
-        d += (v1[i]-v2[i])*(v1[i]-v2[i])
-    return d
+
+def get_first_order_moment(vectors):
+    moments = [float(vectors[0][j]) for j in range(0,len(vectors[0]))]
+    for i in range(1,len(vectors)):
+        for j in range(0,len(vectors[i])):
+            moments[j] += vectors[i][j]
+    for j in range(0,len(moments)):
+        moments[j] /= len(vectors)
+    return moments
+
+def get_second_order_moment(vectors):
+    moments = [float(vectors[0][j]*vectors[0][j]) for j in range(0,len(vectors[0]))]
+    for i in range(1,len(vectors)):
+        for j in range(0,len(vectors[i])):
+            moments[j] += vectors[i][j]*vectors[i][j]
+    for j in range(0,len(moments)):
+        moments[j] /= len(vectors)
+    return moments
+
+#fast avg squared Euclidean distance between all pairs of vectors in the two given sets
+def fast_avg_euclidean_dist(vectors1,vectors2):
+    first_order_moment1 = get_first_order_moment(vectors1)
+    first_order_moment2 = get_first_order_moment(vectors2)
+    second_order_moment1 = get_second_order_moment(vectors1)
+    second_order_moment2 = get_second_order_moment(vectors2)
+
+    dist = 0.0
+    for j in range(0,len(first_order_moment1)):
+        dist += second_order_moment1[j] - 2*first_order_moment1[j]*first_order_moment2[j] + second_order_moment2[j]
+    return dist
+
+def fast_avg_euclidean_dist_momentsgiven(first_order_moment1,first_order_moment2,second_order_moment1,second_order_moment2):
+    dist = 0.0
+    for j in range(0,len(first_order_moment1)):
+        dist += second_order_moment1[j] - 2*first_order_moment1[j]*first_order_moment2[j] + second_order_moment2[j]
+    return dist
 
 #area of the minimum bounding hyper-rectangle
 def mbr_area(embeddings):
-    min_coordinates = []
-    max_coordinates = []
-    for i in range(0,len(embeddings[0])):
-        min_coordinates[i] = embeddings[0][i]
-        max_coordinates[i] = embeddings[0][i]
+    min_coordinates = [embeddings[0][i] for i in range(0,len(embeddings[0]))]
+    max_coordinates = [embeddings[0][i] for i in range(0,len(embeddings[0]))]
     for e in embeddings:
         for i in range(1,len(e)):
             if e[i] < min_coordinates[i]:
@@ -28,6 +55,14 @@ def mbr_area(embeddings):
 
     return area
 
+"""
+#squared Euclidean distance
+def get_dist(v1,v2):
+    d = 0.0
+    for i in range(0,len(v1)):
+        d += (v1[i]-v2[i])*(v1[i]-v2[i])
+    return d
+
 def pairwise_dist(src_ids,target_ids,src_embeddings,target_embeddings):
     dist = {}
     for x_id in src_ids:
@@ -37,41 +72,6 @@ def pairwise_dist(src_ids,target_ids,src_embeddings,target_embeddings):
             d = get_dist(x_embedding,y_embedding)
             dist[(x_id,y_id)] = d
     return dist
-
-"""
-def get_avg_dist(src_obj,target_obj,embeddings,obj2id):
-    dist_sum = 0.0
-    count = 0
-    for i in range(0,len(obj)-1):
-        i_id = obj2id[obj[i]]
-        for j in range(i+1,len(obj)):
-            j_id = obj2id[obj[j]]
-            d = get_dist(embeddings[i_id],embeddings[j_id])
-            count += 1
-            dist_sum += d
-    avg_dist = 0.0 if count == 0 else dist_sum/count
-    return avg_dist
-"""
-
-"""
-def get_avg_dist(src_obj,target_obj,embeddings,obj2id):
-    dist_sum = 0.0
-    count = 0
-    for x in src_obj:
-        dist_sum_x = 0.0
-        x_id = obj2id[x]
-        count_x = 0
-        for y in target_obj:
-            y_id = obj2id[y]
-            d = get_dist(embeddings[x_id],embeddings[y_id])
-            count_x += 1
-            dist_sum_x += d
-        dist_avg_x = 0.0 if count_x == 0 else dist_sum_x/count_x
-        dist_sum += dist_avg_x
-        count += 1
-    dist_avg = 0.0 if count == 0 else dist_sum/count
-    return dist_avg
-"""
 
 def avg_dist(src_ids,target_ids,pairwise_dist):
     dist_sum = 0.0
@@ -87,8 +87,17 @@ def avg_dist(src_ids,target_ids,pairwise_dist):
         count += 1
     dist_avg = 0.0 if count == 0 else dist_sum/count
     return dist_avg
+"""
+
+def debug():
+    vectors = [[1,2,3],[3,2,1],[2,2,2]]
+    print(get_first_order_moment(vectors))
+    print(get_second_order_moment(vectors))
+    print(fast_avg_euclidean_dist([[2,2,3],[1,3,4]],[[2,2,3],[1,3,4]]))
+    sys.exit(-1)
 
 if __name__ == '__main__':
+    #debug()
     rule_support_file = None
     embedding_file = None
     kg_folder = None
@@ -118,16 +127,11 @@ if __name__ == '__main__':
     (entity2id, relation2id) = load.load_openke_dataset(kg_folder)
     print('OpenKE dataset successfully loaded!')
 
-    all_entities = list(entity2id.keys())
-    all_entity_ids = list(entity2id.values())
-    all_relations = list(relation2id.keys())
-    all_relation_ids = list(relation2id.values())
-    print('Computing r2r pairwise distances')
-    rr_pairwise_dist = pairwise_dist(all_relation_ids,all_relation_ids,rel_embeddings,rel_embeddings)
-    print('Computing e2r pairwise distances')
-    er_pairwise_dist = pairwise_dist(all_entity_ids,all_relation_ids,ent_embeddings,rel_embeddings)
-    print('Computing e2e pairwise distances')
-    ee_pairwise_dist = pairwise_dist(all_entity_ids,all_entity_ids,ent_embeddings,ent_embeddings)
+    print('Computing (first-order and second-order) moments of all entities and relations')
+    fo_moment_alle = get_first_order_moment(ent_embeddings)
+    so_moment_alle = get_second_order_moment(ent_embeddings)
+    fo_moment_allr = get_first_order_moment(rel_embeddings)
+    so_moment_allr = get_second_order_moment(rel_embeddings)
 
     print('Computing entity MBRs')
     alle_mbr_area = mbr_area(ent_embeddings)
@@ -156,25 +160,28 @@ if __name__ == '__main__':
             example = tokens[1]
             entities = tokens[2].split()
             relations = tokens[3].split()
-            entity_ids = [entity2id[e] for e in entities]
-            relation_ids = [relation2id[r] for r in entities]
 
-            e2e_avgdist = avg_dist(entity_ids,entity_ids,ee_pairwise_dist)
-            e2alle_avgdist = avg_dist(entity_ids,all_entity_ids,ee_pairwise_dist)
-            e2r_avgdist = avg_dist(entity_ids,relation_ids,er_pairwise_dist)
-            e2allr_avgdist = avg_dist(entity_ids,all_relation_ids,er_pairwise_dist)
-            r2r_avgdist = avg_dist(relation_ids,relation_ids,rr_pairwise_dist)
-            r2allr_avgdist = avg_dist(relation_ids,all_relation_ids,rr_pairwise_dist)
+            current_ent_embeddings = [ent_embeddings[entity2id[e]] for e in entities]
+            fo_moment_currente = get_first_order_moment(current_ent_embeddings)
+            so_moment_currente = get_second_order_moment(current_ent_embeddings)
+            current_rel_embeddings = [rel_embeddings[relation2id[r]] for r in relations]
+            fo_moment_currentr = get_first_order_moment(current_rel_embeddings)
+            so_moment_currentr = get_second_order_moment(current_rel_embeddings)
 
-            e_embeddings = [ent_embeddings[e_id] for e_id in entity_ids]
-            r_embeddings = [rel_embeddings[r_id] for r_id in relation_ids]
-            e_mbr_area = mbr_area(e_embeddings)
-            er_mbr_area = mbr_area(e_embeddings+r_embeddings)
-            r_mbr_area = mbr_area(r_ambeddings)
+            e2e_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currente,fo_moment_currente,so_moment_currente,so_moment_currente)
+            e2alle_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currente,fo_moment_alle,so_moment_currente,so_moment_alle)
+            e2r_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currente,fo_moment_currentr,so_moment_currente,so_moment_currentr)
+            e2allr_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currente,fo_moment_allr,so_moment_currente,so_moment_allr)
+            r2r_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currentr,fo_moment_currentr,so_moment_currentr,so_moment_currentr)
+            r2allr_avgdist = fast_avg_euclidean_dist_momentsgiven(fo_moment_currentr,fo_moment_allr,so_moment_currentr,so_moment_allr)
 
-            output_line = '\t'.join([rule,example,e2e_avgdist,e2alle_avgdist,e2r_avgdist,e2allr_avgdist,r2r_avgdist,r2allr_avgdist,e_mbr_area,alle_mbr_area,er_mbr_area,aller_mbr_area,r_mbr_area,allr_mbr_area])
+            e_mbr_area = mbr_area(current_ent_embeddings)
+            er_mbr_area = mbr_area(current_ent_embeddings+current_rel_embeddings)
+            r_mbr_area = mbr_area(current_rel_embeddings)
+
+            output_line = '\t'.join([rule,example,str(e2e_avgdist),str(e2alle_avgdist),str(e2r_avgdist),str(e2allr_avgdist),str(r2r_avgdist),str(r2allr_avgdist),str(e_mbr_area),str(alle_mbr_area),str(er_mbr_area),str(aller_mbr_area),str(r_mbr_area),str(allr_mbr_area)])
             #print(rule + '\t' + example + '\t' + str(avg_dist_ent2ent) + '\t' + str(avg_dist_ent2row) + '\t' + str(avg_dist_rel2rel) + '\t' + str(avg_dist_rel2row))
-            output.write(output_line)
+            output.write('\n' + output_line)
             output.flush()
             line = rules.readline()
             count += 1
